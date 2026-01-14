6726c5488cd2c01465df7c94b50106f41cd69d74
Rohit Gupta <rohitgupta122343@gmail.com>
2026-01-14 14:49:04 -0800

    Create app.js

---
+++ SwapTwoNumber/app.js
@@ -0,0 +1,18 @@
+
+let a = 10; 
+let b = 20;
+let temp;
+
+// 1st  Method
+
+// temp = a;
+// a = b;
+// b =temp;
+// console.log(`a = ${a} b =  ${b}`);
+
+// 2nd Method 
+
+// [a,b] = [b,a];
+// console.log(`a = ${a} b =  ${b}`);
+
+
