66feb091e10e1458fe2dffd9c31b83ec69ab7c10
Rohit Gupta <rohitgupta122343@gmail.com>
2026-04-01 04:16:36 -0700

    add

---
+++ Swaping/app.js
@@ -0,0 +1,23 @@
+
+let a = 10;
+let b = 20;
+let c;
+
+// normal 3 var used
+
+// c = a;
+// a = b;
+// b = c;
+
+// destructure
+// [a,b] = [b,a]
+
+
+// using logic
+
+// a = a+b;
+// b = a-b;
+// a = a-b;
+
+
+console.log(a,b);
